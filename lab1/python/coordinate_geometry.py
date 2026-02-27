64bb8db1fa31c17fa449ccfa893c6ce452e316fb
Irmak Al <168216554+irmakall@users.noreply.github.com>
2026-02-27 17:13:13 +0300

    Update coordinate_geometry.py

--- lab1/python/coordinate_geometry.py
+++ lab1/python/coordinate_geometry.py
@@ -1,10 +1,10 @@
 import math
 
-point1_x = int(input("Enter a point 1: "))
-point1_y = int(input("Enter a point 1: "))
+point1_x = int(input("Enter for point 1 x: "))
+point1_y = int(input("Enter for point 1 y: "))
 
-point2_x = int(input("Enter a point 2: "))
-point2_y = int(input("Enter a point 2: "))
+point2_x = int(input("Enter for point 2 x: "))
+point2_y = int(input("Enter for point 2 y: "))
 
 result = math.sqrt((point1_x - point2_x)**2 + (point1_y - point2_y)**2)
 print(result)
